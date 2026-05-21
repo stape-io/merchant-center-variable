0cdadb14685deacc3744b4a59ba5f586aab5b81f
giovaniortolani <9199996+giovaniortolani@users.noreply.github.com>
2026-05-21 15:14:31 -0300

    Console and BigQuery logging removal

--- template.js
+++ template.js
@@ -1,12 +1,8 @@
-const BigQuery = require('BigQuery');
 const encodeUriComponent = require('encodeUriComponent');
-const getContainerVersion = require('getContainerVersion');
 const getGoogleAuth = require('getGoogleAuth');
-const getRequestHeader = require('getRequestHeader');
 const getTimestampMillis = require('getTimestampMillis');
 const getType = require('getType');
 const JSON = require('JSON');
-const logToConsole = require('logToConsole');
 const makeNumber = require('makeNumber');
 const makeString = require('makeString');
 const Promise = require('Promise');
@@ -50,28 +46,12 @@ function getData(item) {
     ':' +
     enc(item[itemIdKey]);
 
-  log({
-    Name: 'GoogleMerchantCenterLookup',
-    Type: 'Request',
-    EventName: 'LookupRequest',
-    RequestMethod: 'GET',
-    RequestUrl: url
-  });
-
   const auth = getGoogleAuth({
     scopes: ['https://www.googleapis.com/auth/content']
   });
 
   return sendHttpRequest(url, { method: 'GET', authorization: auth }).then(
     (result) => {
-      log({
-        Name: 'GoogleMerchantCenterLookup',
-        Type: 'Response',
-        EventName: 'LookupRequest',
-        ResponseStatusCode: result.statusCode,
-        ResponseHeaders: result.headers,
-        ResponseBody: result.body
-      });
       const result_data = JSON.parse(result.body || '{}');
       if (result.statusCode >= 200 && result.statusCode < 300) {
         if (enableItemMatchStatus) item.merchant_center_status = 'match';
@@ -86,13 +66,6 @@ function getData(item) {
       return item;
     },
     (result) => {
-      log({
-        Name: 'GoogleMerchantCenterLookup',
-        Type: 'Message',
-        EventName: 'LookupRequest',
-        Message: 'Some request may have failed or timed out.',
-        Reason: JSON.stringify(result)
-      });
       if (enableItemMatchStatus) item.merchant_center_status = 'api_error';
       return item;
     }
@@ -126,92 +99,6 @@ function mapResultVariables(item, result_data, mapping) {
 /*==============================================================================
   Helpers
 ==============================================================================*/
-
-function log(rawDataToLog) {
-  const logDestinationsHandlers = {};
-  if (determinateIsLoggingEnabled()) logDestinationsHandlers.console = logConsole;
-  if (determinateIsLoggingEnabledForBigQuery()) logDestinationsHandlers.bigQuery = logToBigQuery;
-
-  rawDataToLog.TraceId = getRequestHeader('trace-id');
-
-  const keyMappings = {
-    // No transformation for Console is needed.
-    bigQuery: {
-      Name: 'tag_name',
-      Type: 'type',
-      TraceId: 'trace_id',
-      EventName: 'event_name',
-      RequestMethod: 'request_method',
-      RequestUrl: 'request_url',
-      RequestBody: 'request_body',
-      ResponseStatusCode: 'response_status_code',
-      ResponseHeaders: 'response_headers',
-      ResponseBody: 'response_body'
-    }
-  };
-
-  for (const logDestination in logDestinationsHandlers) {
-    const handler = logDestinationsHandlers[logDestination];
-    if (!handler) continue;
-
-    const mapping = keyMappings[logDestination];
-    const dataToLog = mapping ? {} : rawDataToLog;
-
-    if (mapping) {
-      for (const key in rawDataToLog) {
-        const mappedKey = mapping[key] || key;
-        dataToLog[mappedKey] = rawDataToLog[key];
-      }
-    }
-
-    handler(dataToLog);
-  }
-}
-
-function logConsole(dataToLog) {
-  logToConsole(JSON.stringify(dataToLog));
-}
-
-function logToBigQuery(dataToLog) {
-  const connectionInfo = {
-    projectId: data.logBigQueryProjectId,
-    datasetId: data.logBigQueryDatasetId,
-    tableId: data.logBigQueryTableId
-  };
-  dataToLog.timestamp = getTimestampMillis();
-
-  ['request_body', 'response_headers', 'response_body'].forEach((p) => {
-    dataToLog[p] = JSON.stringify(dataToLog[p]);
-  });
-  BigQuery.insert(connectionInfo, [dataToLog], { ignoreUnknownValues: true });
-}
-
-function determinateIsLoggingEnabled() {
-  const containerVersion = getContainerVersion();
-  const isDebug = !!(
-    containerVersion &&
-    (containerVersion.debugMode || containerVersion.previewMode)
-  );
-
-  if (!data.logType) {
-    return isDebug;
-  }
-
-  if (data.logType === 'no') {
-    return false;
-  }
-
-  if (data.logType === 'debug') {
-    return isDebug;
-  }
-
-  return data.logType === 'always';
-}
-
-function determinateIsLoggingEnabledForBigQuery() {
-  if (data.bigQueryLogType === 'no') return false;
-  return data.bigQueryLogType === 'always';
-}
 
 function enc(data) {
   if (['null', 'undefined'].indexOf(getType(data)) !== -1) data = '';
