506b7d8d247171e4f4ba69298304fcbdc5952f29
giovaniortolani <9199996+giovaniortolani@users.noreply.github.com>
2026-06-25 18:44:23 -0300

    feat: migrate to Merchant API, improve UI and code quality
        - Add README migration guide and in-template API migration warning
        - Update Basic Mapping to use Merchant API field names (ageGroup,
          isBundle, gtins) and add new attributes (legacyLocal, dataSource,
          archived, versionNumber)
        - Look up values from productAttributes with root object fallback
        - Fix productTypes mapping, cache key uniqueness, item reassignment
        - Rename/scope variables, add isValidValue(), fix encoding chars
        - UI: rename labels to "attribute", update help texts, reorder fields

--- template.js
+++ template.js
@@ -1,4 +1,6 @@
+const createRegex = require('createRegex');
 const encodeUriComponent = require('encodeUriComponent');
+const getEventData = require('getEventData');
 const getGoogleAuth = require('getGoogleAuth');
 const getTimestampMillis = require('getTimestampMillis');
 const getType = require('getType');
@@ -8,56 +10,77 @@ const makeString = require('makeString');
 const Promise = require('Promise');
 const sendHttpRequest = require('sendHttpRequest');
 const templateDataStorage = require('templateDataStorage');
+const toBase64 = require('toBase64');
 
 /*==============================================================================
 ==============================================================================*/
 
-const cache = makeNumber(data.cache) * 60 * 60 * 1000;
-const feed_identifier = data.feed_language + '_' + data.feed_label;
+const API_VERSION = '1';
 const itemIdKey = data.itemIdKey ? data.itemIdKey : 'item_id';
-const enableItemMatchStatus = data.enable_item_match_status;
-
-const items = data.items;
+const items = data.useGA4Items ? getEventData('items') : data.items;
 
 if (getType(items) !== 'array') return undefined;
 
-return Promise.all(items.map(getData));
+return Promise.all(items.map(getItemData));
 
 /*==============================================================================
   Vendor related functions
 ==============================================================================*/
 
-function getData(item) {
-  const storageKey = feed_identifier + item[itemIdKey];
-  const cachedItem = templateDataStorage.getItemCopy(storageKey);
-  if (cachedItem && cachedItem.ts + cache > getTimestampMillis()) {
+function getMerchantApiRequestUrl(item) {
+  const merchantId = enc(data.merchant_center_id);
+  const feedLanguage = makeString(data.feed_language);
+  const feedLabel = makeString(data.feed_label);
+  const itemId = makeString(item[itemIdKey]);
+  const baseUrl = 'https://merchantapi.googleapis.com/products/v' + API_VERSION + '/';
+
+  let productStringPath = feedLanguage + '~' + feedLabel + '~' + itemId;
+  if (productStringPathNeedsEncoding(feedLanguage + feedLabel + itemId)) {
+    productStringPath = encodeForMerchantApi(productStringPath);
+  }
+
+  const productPath = ['accounts', merchantId, 'products', productStringPath].join('/');
+  return baseUrl + productPath;
+}
+
+function productStringPathNeedsEncoding(string) {
+  // URI reserved characters
+  if (string.match('[\\.\\:\\,\\(\\)\\*\\!\\/\\&\\?\\#\\=\\%\\~\\@\\+\\$]')) return true;
+  return false;
+}
+
+function encodeForMerchantApi(string) {
+  if (!string) return '';
+  const paddingRegex = createRegex('=*', 'ig');
+  const encodedString = toBase64(string, { urlEncoding: true });
+  return encodedString.replace(paddingRegex, '');
+}
+
+function getItemData(item) {
+  const enableItemMatchStatus = data.enable_item_match_status;
+
+  const feedIdentifier = data.merchant_center_id + '_' + data.feed_language + '_' + data.feed_label;
+  const storageKey = feedIdentifier + item[itemIdKey];
+  const cacheExpirationTime = makeNumber(data.cache) * 60 * 60 * 1000;
+  const cachedResultData = templateDataStorage.getItemCopy(storageKey);
+  if (cachedResultData && cachedResultData.ts + cacheExpirationTime > getTimestampMillis()) {
     if (enableItemMatchStatus) item.merchant_center_status = 'match';
-    mapResult(item, cachedItem);
+    item = mapResult(item, cachedResultData);
     return item;
   }
 
-  const url =
-    'https://shoppingcontent.googleapis.com/content/v2.1/' +
-    enc(data.merchant_center_id) +
-    '/products/online:' +
-    enc(data.feed_language) +
-    ':' +
-    enc(data.feed_label) +
-    ':' +
-    enc(item[itemIdKey]);
-
+  const merchantApiUrl = getMerchantApiRequestUrl(item);
   const auth = getGoogleAuth({
     scopes: ['https://www.googleapis.com/auth/content']
   });
-
-  return sendHttpRequest(url, { method: 'GET', authorization: auth }).then(
+  return sendHttpRequest(merchantApiUrl, { method: 'GET', authorization: auth }).then(
     (result) => {
-      const result_data = JSON.parse(result.body || '{}');
+      const resultData = JSON.parse(result.body || '{}');
       if (result.statusCode >= 200 && result.statusCode < 300) {
         if (enableItemMatchStatus) item.merchant_center_status = 'match';
-        result_data.ts = getTimestampMillis();
-        templateDataStorage.setItemCopy(storageKey, result_data);
-        mapResult(item, result_data);
+        resultData.ts = getTimestampMillis();
+        templateDataStorage.setItemCopy(storageKey, resultData);
+        item = mapResult(item, resultData);
       } else if (result.statusCode === 404) {
         if (enableItemMatchStatus) item.merchant_center_status = 'no_match';
       } else {
@@ -72,12 +95,13 @@ function getData(item) {
   );
 }
 
-function mapResult(item, result_data) {
-  mapResultVariables(item, result_data, data.mapping_basic);
-  mapResultVariables(item, result_data, data.mapping_custom);
+function mapResult(item, resultData) {
+  item = mapResultVariables(item, resultData, data.mapping_basic);
+  item = mapResultVariables(item, resultData, data.mapping_custom);
 
-  if (data.map_categories && result_data.productTypes) {
-    result_data.productTypes.forEach((productType, index) => {
+  const productTypes = (resultData.productAttributes || {}).productTypes || resultData.productTypes; // Backward compatibility.
+  if (data.map_categories && getType(productTypes) === 'array') {
+    productTypes.forEach((productType, index) => {
       const itemCategoryIndex = index !== 0 ? index + 1 : '';
       item['item_category' + itemCategoryIndex] = productType;
     });
@@ -86,14 +110,50 @@ function mapResult(item, result_data) {
   return item;
 }
 
-function mapResultVariables(item, result_data, mapping) {
-  if (getType(mapping) !== 'array') return;
+function mapResultVariables(item, resultData, mapping) {
+  if (getType(mapping) !== 'array') return item;
 
-  for (let i = 0; i < mapping.length; i++) {
-    const mappingItem = mapping[i];
-    const value = result_data[mappingItem.merchant_center_variable];
-    if (value) item[mappingItem.item_variable] = value;
-  }
+  const contentApiToMerchantApiDictionary = {
+    // Top-level attributes
+    id: 'name',
+    targetCountry: 'feedLabel',
+    // .productAttributes attributes
+    gtin: 'gtins'
+  };
+
+  mapping.forEach((mappingItem) => {
+    const keyForMerchant = mappingItem.merchant_center_variable;
+    const activeKey = contentApiToMerchantApiDictionary[keyForMerchant] || keyForMerchant || '';
+    const activeKeyPathArray = activeKey.split('.');
+    const usesDotNotation = activeKeyPathArray.length > 1;
+    const productAttributes = resultData.productAttributes || {};
+
+    let value;
+    // Backward compatibility for old Content API fields in the UI.
+    // For the new API, field names are mutually exclusive. If not using dot notation, check productAttributes first, and if the attribute is not found, check the top-level resultData.
+    if (!usesDotNotation) {
+      value =
+        getType(productAttributes[activeKey]) !== 'undefined'
+          ? productAttributes[activeKey]
+          : resultData[activeKey];
+    } else {
+      value = getNestedValue(resultData, activeKeyPathArray);
+    }
+
+    if (
+      (keyForMerchant === 'gtin' /* Backward compatibility */ ||
+        keyForMerchant ===
+          'gtins') /* New API, but we still return only one element of the array */ &&
+      getType(value) === 'array'
+    ) {
+      value = value[0];
+    }
+
+    const keyForGAItems = mappingItem.item_variable;
+    if (isValidValue(value)) item[keyForGAItems] = value;
+  });
+
+  return item;
 }
 
 /*==============================================================================
@@ -103,4 +163,15 @@ function mapResultVariables(item, result_data, mapping) {
 function enc(data) {
   if (['null', 'undefined'].indexOf(getType(data)) !== -1) data = '';
   return encodeUriComponent(makeString(data));
+}
+
+function isValidValue(value) {
+  const valueType = getType(value);
+  return valueType !== 'null' && valueType !== 'undefined' && value !== '' && value === value;
+}
+
+function getNestedValue(obj, pathArray) {
+  return pathArray.reduce((acc, key) => {
+    return getType(acc) !== 'undefined' && getType(acc) !== 'null' ? acc[key] : undefined;
+  }, obj);
 }
